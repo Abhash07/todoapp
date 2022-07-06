443443146545f2976b2c4ee1a1d5895d316b2043
Abhash Sapkota <abhash007@Abhashs-MacBook-Pro.local>
2022-07-06 19:51:33 +0545

    task complete

---
+++ src/components/todo-form/todo-form.jsx
@@ -0,0 +1,44 @@
+// import { FALSE } from 'node-sass';
+import * as React from 'react';
+import { TodosContext } from '../../todo-context';
+import './todo-form.scss';
+
+export const TodoForm = () => {
+  const { todos, setTodos } = React.useContext(TodosContext);
+  const initialTask = {
+    id: '',
+    checked: false,
+    label: '',
+  };
+  const [task, setTask] = React.useState(initialTask);
+
+  const handleAddTodo = () => {
+   setTodos((prev) => ([...prev, task]));
+   setTask(initialTask);
+  };
+
+  const handleKeyUp = (e) => {
+    e.preventDefault();
+    if (e.keyCode === 13) {
+      handleAddTodo();
+    }
+  };
+
+  return (
+    <div className="todo-form">
+      <input
+        placeholder="Enter new task"
+        value={task.label}
+        onChange={(e) => setTask({
+          ...task,
+          id: todos.length,
+          label: e.target.value,
+        })}
+        onKeyUp={handleKeyUp}
+      />
+      <button type="button" onClick={handleAddTodo}>
+        Add task
+      </button>
+    </div>
+  );
+};
---
+++ src/components/todo-list/todo-list.jsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { Checkbox } from '../checkbox';
+import { TodosContext } from '../../todo-context';
+import './todo-list.scss';
+
+export const TodoList = () => {
+  const { todos, setTodos } = React.useContext(TodosContext);
+
+  const handleDelete = (id) => {
+   const newTodos = todos.filter((task) => task.id !== id);
+   setTodos(newTodos);
+  };
+
+  const toggleCheck = (id) => {
+  todos.map((task) => {
+    if (task.id === id) {
+      const updatedTask = {
+        ...task,
+        checked: !task.checked,
+      };
+      const newTodos = todos.filter((todo) => todo.id !== id);
+      setTodos([...newTodos, updatedTask]);
+    }
+  return task;
+});
+  };
+
+  const handleKeyUp = (e, id) => {
+    if (e.keyCode === 13) {
+      toggleCheck(id);
+    }
+  };
+
+  return (
+    <div className="todo-list">
+      <span className="todo-list-title">Things to do:</span>
+      {todos.length ? (
+        <div className="todo-list-content">
+          {todos.map((todoItem) => (
+            <Checkbox
+              key={todoItem.id}
+              label={todoItem.label}
+              checked={todoItem.checked}
+              onClick={() => toggleCheck(todoItem.id)}
+              onKeyUp={(e) => handleKeyUp(e, todoItem.id)}
+              onDelete={() => handleDelete(todoItem.id)}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="no-todos">Looks like you&apos;re absolutely free today!</div>
+      )}
+    </div>
+  );
+};
